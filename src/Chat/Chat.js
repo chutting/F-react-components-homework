51397653a10594b4a9ad6fd679e4bc811b9092f1
chutting <tingting.chu@thoughtworks.com>
2020-11-25 16:24:11 +0800

    [Chu] complete the page

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -5,6 +5,7 @@ import ChatBox from './ChatBox/ChatBox';
 import ChatInput from './ChatInput/ChatInput';
 import shopData from '../data/shop.json';
 import answersData from '../data/answers.json';
+import { ROLE } from '../constants';
 
 class Chat extends Component {
   constructor(props, context) {
@@ -27,13 +28,38 @@ class Chat extends Component {
     }, 1000);
   }
 
+  handleChangleMessage = (message) => {
+    this.addMessage({ text: message, role: ROLE.CUSTOMER });
+    const response = answersData.filter((answer) => {
+      const matchTag = answer.tags.find((tag) => message.includes(tag));
+      return !!matchTag;
+    });
+    this.addMessage(response);
+  };
+
+  addMessage = (message) => {
+    const messages = this.state.messages.concat(message);
+    if (message.role === ROLE.CUSTOMER) {
+      this.setState({
+        messages,
+      });
+    } else {
+      setTimeout(() => {
+        this.setState({
+          messages,
+        });
+      }, 1000);
+    }
+  };
+
   render() {
+    console.log(this.state.messages);
     const { shop, messages } = this.state;
     return (
       <main className="Chat">
         <ChatHeader shop={shop} />
         <ChatBox messages={messages} />
-        <ChatInput />
+        <ChatInput changeMessage={(message) => this.handleChangleMessage(message)} />
       </main>
     );
   }
--- src/Chat/ChatInput/ChatInput.js
+++ src/Chat/ChatInput/ChatInput.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import './ChatInput.scss';
 
 class ChatInput extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      customerMessage: '',
+    };
+  }
+
+  handleSubmit = () => {
+    this.props.changeMessage(this.state.customerMessage);
+    document.querySelector('.input').value = '';
+  };
+
+  handleChangeMessage = (e) => {
+    this.setState({
+      customerMessage: e.target.value,
+    });
+  };
+
   render() {
     return (
       <footer className="ChatInput">
-        <input type="text" />
-        <button type="button">Send</button>
+        <input type="text" onBlur={this.handleChangeMessage} className="input" />
+        <button type="button" onClick={this.handleSubmit}>
+          Send
+        </button>
       </footer>
     );
   }
